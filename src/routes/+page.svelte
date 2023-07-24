fbda06414ec4e7b362510c082808b5d93127a62f
Stefanie Voelker <stefanie.voelker.ext@siemens-healthineers.com>
2023-07-24 17:02:32 +0200

    style: :lipstick: style background of countries

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,11 +12,11 @@
     let allCountries : Promise<Country[]> = fetch('https://restcountries.com/v3.1/all').then( (result) => result.json());
 </script>
 
-<div>
+<div class="flex items-center bg-white rounded-xl shadow-lg max-w-3xl">
     {#await allCountries then data}
         <ul class="list-inside">
             {#each data as country}
-                <li class="font-medium py-6 text-center">
+                <li class="font-medium py-6 text-center content-center">
                     <div class="py-2">
 						<p class="text-2xl">{country.name.common}</p>
 						<p class="text-m">({country.name.official})</p>
