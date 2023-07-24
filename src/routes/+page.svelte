6c9b44eb74abf9abdc4d6642725dfec050559c52
Stefanie Voelker <stefanie.voelker.ext@siemens-healthineers.com>
2023-07-24 16:42:30 +0200

    feat: :sparkles: fetch and print all countries

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    interface Country{
+        name: {
+            common: string,
+            official: string
+        },
+        flags: {
+            png: string
+        }
+    }
+
+    let allCountries : Promise<Country[]> = fetch('https://restcountries.com/v3.1/all').then( (result) => result.json());
+</script>
+
+<div>
+    {#await allCountries then data}
+        <ul>
+            {#each data as country}
+                <li>
+                    <div class="py-2">
+						<p class="text-2xl">{country.name.common}</p>
+						<p class="text-m">({country.name.official})</p>
+					</div>
+                </li>                
+            {/each}
+        </ul>
+    {/await}
+</div>
