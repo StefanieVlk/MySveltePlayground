f06fa5f30a20da1ac71c89bf498090cc675a696e
Stefanie Voelker <stefanie.voelker.ext@siemens-healthineers.com>
2023-07-24 17:07:03 +0200

    style: :lipstick: text styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,9 +16,9 @@
     {#await allCountries then data}
         <ul class="list-inside">
             {#each data as country}
-                <li class="font-medium py-6 text-center content-center">
+                <li class="py-6 text-center content-center">
                     <div class="py-2">
-						<p class="text-2xl">{country.name.common}</p>
+						<p class="font-medium text-2xl">{country.name.common}</p>
 						<p class="text-m">({country.name.official})</p>
 					</div>
                     <div class="py-2">
